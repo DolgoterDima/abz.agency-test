dea09f965ea062315205041e99fa79d7502bed80
DDolgy <dmitriy.d@filancy.com>
2023-03-20 13:53:38 +0200

    feat: homepage

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  /*  baseUrl:
+    process.env.NODE_ENV === "production" ? "/abz.agency-test-task/" : "/",*/
   publicPath:
     process.env.NODE_ENV === "production" ? "/abz.agency-test-task/" : "/",
   transpileDependencies: true,
